ae8624ec5bacad196164dd965cb12013f74df159
renegumroad <174652038+renegumroad@users.noreply.github.com>
2024-07-10 10:28:18 -0400

    New filesystem and systeminfo utils packages (#6)
    * New filesystem and systeminfo utils packages
    
    * Fix log.Fatalf implementation

---
+++ internal/utils/systeminfo/systeminfo.go
@@ -0,0 +1,84 @@
+package systeminfo
+
+import (
+	"os"
+	"os/user"
+	"runtime"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+type Client interface {
+	IsLinux() bool
+	IsMacOS() bool
+	IsWindows() bool
+	GetSudoOriginalUser() (*userInfo, error)
+}
+
+type client struct {
+	user userHandler
+}
+
+type userHandler interface {
+	Lookup(username string) (*user.User, error)
+}
+
+type userImpl struct {
+}
+
+type userInfo struct {
+	Id   int
+	Name string
+}
+
+func NewClient() Client {
+	return newClientWithComponents(newUserHandler())
+}
+
+func newClientWithComponents(user userHandler) Client {
+	return &client{
+		user: user,
+	}
+}
+
+func (c *client) IsLinux() bool {
+	return runtime.GOOS == "linux"
+}
+
+func (c *client) IsMacOS() bool {
+	return runtime.GOOS == "darwin"
+}
+
+func (c *client) IsWindows() bool {
+	return runtime.GOOS == "windows"
+}
+
+func (c *client) GetSudoOriginalUser() (*userInfo, error) {
+	sudoUsername := os.Getenv("SUDO_USER")
+	if sudoUsername == "" {
+		return nil, errors.Errorf("Not running with sudo or SUDO_USER is not set")
+	}
+
+	info := &userInfo{}
+	originalUser, err := c.user.Lookup(sudoUsername)
+	if err != nil {
+		return nil, errors.Errorf("Unable to get information about user %s: %s", sudoUsername, err)
+	}
+	info.Name = originalUser.Username
+	if userId, err := strconv.Atoi(originalUser.Uid); err != nil {
+		return nil, errors.Errorf("Unable to convert user id to int: %s", err)
+	} else {
+		info.Id = userId
+	}
+
+	return info, nil
+}
+
+func newUserHandler() userHandler {
+	return &userImpl{}
+}
+
+func (u *userImpl) Lookup(username string) (*user.User, error) {
+	return user.Lookup(username)
+}
---
+++ internal/utils/systeminfo/systeminfo_test.go
@@ -0,0 +1,65 @@
+package systeminfo
+
+import (
+	"os"
+	"os/user"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type systeminfoSuite struct {
+	suite.Suite
+}
+
+// Mock for the os/user.Lookup function
+type UserLookupMock struct {
+	mock.Mock
+}
+
+func (m *UserLookupMock) Lookup(username string) (*user.User, error) {
+	args := m.Called(username)
+	return args.Get(0).(*user.User), args.Error(1)
+}
+
+type SystemInfoSuite struct {
+	suite.Suite
+}
+
+func (s *systeminfoSuite) TestGetSudoOriginalUserWithSudo() {
+	// Set SUDO_USER environment variable
+	os.Setenv("SUDO_USER", "testuser")
+	defer os.Unsetenv("SUDO_USER")
+
+	// Mock user.Lookup to return a specific user
+	expectedUser := &user.User{Username: "testuser", Uid: "1001"}
+	userMock := &UserLookupMock{}
+	userMock.On("Lookup", "testuser").Return(expectedUser, nil)
+
+	client := newClientWithComponents(userMock)
+	userInfo, err := client.GetSudoOriginalUser()
+
+	s.Require().NoError(err)
+	s.Require().NotNil(userInfo)
+	s.Require().Equal("testuser", userInfo.Name)
+	s.Require().Equal(1001, userInfo.Id)
+
+	userMock.AssertExpectations(s.T())
+}
+
+func (s *systeminfoSuite) TestGetSudoOriginalUserWithoutSudo() {
+	// Ensure SUDO_USER environment variable is not set
+	os.Unsetenv("SUDO_USER")
+
+	client := NewClient()
+	userInfo, err := client.GetSudoOriginalUser()
+
+	s.Require().Error(err)
+	s.Require().Nil(userInfo)
+	s.Require().EqualError(err, "Not running with sudo or SUDO_USER is not set")
+}
+
+func TestSystemInfoSuite(t *testing.T) {
+	suite.Run(t, new(systeminfoSuite))
+}
